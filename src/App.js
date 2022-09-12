b7d040d586dd4b03a8a415a387669f39b42099df
Lexys Manuel <lexysalonso@gmail.com>
2022-09-12 11:48:39 -0400

    Este es el cuarto commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React lmdiaz manuel diaz
+          Learn React lmdiaz manuel diaz Alonso
         </a>
       </header>
     </div>
