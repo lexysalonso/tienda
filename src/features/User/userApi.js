76a5eef6b5b058ccb1d6bb1afe68c938b80119fb
Lexys Manuel <lexysalonso@gmail.com>
2022-11-01 08:51:48 -0400

    variable de entorno

--- src/features/User/userApi.js
+++ src/features/User/userApi.js
@@ -6,6 +6,7 @@ import Storage from "../../hooks/Storage";
 import toast from "../../components/Toast/Toast";
 
 //const API = "https://fakestoreapi.com/";
+//const API = "https://fakestoreapi.com/";
 let API = process.env.REACT_APP_API; 
 //const API = "https://reqres.in/api/";
 
