5435bcff21f108c7befcd2e8db624a2d862d40fe
Lexys Manuel <lexysalonso@gmail.com>
2022-11-14 14:47:36 -0500

    limpiando el index

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,16 +1,6 @@
 export { default as Login } from "./Login/Login";
 export { default as LandingPages } from "./LandingPages/LandingPages";
 
-import React from "react";
 
-const index = () => {
-  return (
-    <article>
-      <div>
-        <p>asddas</p>
-      </div>
-    </article>
-  );
-};
 
-export default index;
+
